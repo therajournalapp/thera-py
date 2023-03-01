b860d55ad5d925e372de8d8628b563126a09d304
Ethan Pedersen <eped360@gmail.com>
2023-02-28 23:26:46 -0700

    Added sentiment and summarization. Adds asent package

--- index.py
+++ index.py
@@ -1,7 +1,11 @@
 from flask import *
 import en_core_web_sm
 import spacy
-# from spacytextblob.spacytextblob import SpacyTextBlob
+import asent
+from string import punctuation
+from collections import Counter
+from heapq import nlargest
+import json
 
 app = Flask(__name__)
 
@@ -16,30 +20,98 @@ def test():
 
 @app.route("/tags", methods=['POST'])
 def tags():
-    # validation
-    # credential = request.cookies.get('credential')
-    # do something
-
-    # text = request.form['text']
-    text = request.args.get('text')
-
-    ents = nlp(text).ents
+    """Returns suggested tags for a document by a list of named entities and a list of keywords.
+    
+    Tags are returned in the form of a list of tuples where the first element is the text and the second element is the entity tag.
+    Keywords are determined by taking the most common nouns, adjectives, verbs and adverbs and returning the top 5. 
+    This helps to fill in suggestions when no named entities are identified.
+    """
+    text = request.get_json()['text']
+    doc = nlp(text)
+    ents = doc.ents
+    
+    # make tags more robust if no named entities are found
+    keywords = []
+    # only consider nouns, adjectives, verbs and adverbs
+    for token in doc:
+        if(token.is_stop or token.is_punct or token.is_space):
+            continue
+        if(token.pos_ == 'NOUN' or token.pos_ == 'PROPN' or token.pos_ == 'ADJ' or token.pos_ == 'VERB' or token.pos_ == 'ADV'):
+            keywords.append(token.text)
+    freq_word = Counter(keywords)
+    freq_word.most_common(5)
+    
     return {
-        "tags": [(ent.text, ent.label_) for ent in ents]
+        "tags": [(ent.text, ent.label_) for ent in ents],
+        "keywords": [w[0] for w in freq_word.most_common(5)],
     }
 
+@app.route("/summary", methods=['POST'])
+def summary():
+    """Generate a summary of the text
 
-# @app.route("/sentiment", methods=['POST'])
-# def sentiment():
-#     # validation
-#     credential = request.cookies.get('credential')
-#     # do something
+    If text is too short, returns the original text.
+    Otherwise, returns a summary of the text.
+    Credit: https://medium.com/analytics-vidhya/text-summarization-using-spacy-ca4867c6b744
+    """
+    doc = nlp(request.get_json()['text'])
 
-#     text = request.form['text']
-#     processed = nlp(text)
-#     return {
-#         "sentiment": processed._.blob.sentiment_assessments.assessments
-#     }
+    # only do it on more than a few sentences
+    if(len(list(doc.sents)) < 5):
+        return {
+            "summary": doc.text
+        }
+    keywords = []
+    # only consider nouns, adjectives, verbs and adverbs
+    for token in doc:
+        if(token.is_stop or token.is_punct or token.is_space):
+            continue
+        if(token.pos_ == 'NOUN' or token.pos_ == 'PROPN' or token.pos_ == 'ADJ' or token.pos_ == 'VERB' or token.pos_ == 'ADV'):
+            keywords.append(token.text)
+    freq_word = Counter(keywords)
+    freq_word.most_common(5)
+    max_freq = freq_word.most_common(1)[0][1]
+    for word in freq_word.keys():
+        freq_word[word] = (freq_word[word]/max_freq)
+    sent_strength = {}
+    for sent in doc.sents:
+        for word in sent:
+            if word.text in freq_word.keys():
+                if sent in sent_strength.keys():
+                    sent_strength[sent] += freq_word[word.text]
+                else:
+                    sent_strength[sent]=freq_word[word.text]
+                    
+    summary_sentences = nlargest(3, sent_strength, key=sent_strength.get)
+    final_sentences = [w.text for w in summary_sentences]
+    summary = ' '.join(final_sentences)
+    return {
+        "summary" : summary
+    }
+
+@app.route("/sentiment", methods=['POST'])
+def sentiment():
+    """ Returns polarity scores for the text. 
+    
+    A negative, neutral, positive and compound score is returned.
+    Compound score is a normalized score. 
+    """
+    # validation
+    # do something
+
+    text = request.get_json()['text']
+
+    doc = nlp(text)
+    sentiments = [s._.polarity for s in doc.sents]
+    print(doc._.polarity)
+    print(sentiments)
+    polarities = str(doc._.polarity).split(' ')
+    return {
+        "neg": polarities[0][4:],
+        "neu": polarities[1][4:],
+        "pos": polarities[2][4:],
+        "compound": polarities[3][9:],
+    }
 
 if __name__ == '__main__':
     # change the nltk data directory
@@ -51,7 +123,8 @@ if __name__ == '__main__':
 
     # nlp = spacy.load('en_core_web_sm')
     nlp = en_core_web_sm.load()
+    nlp.add_pipe('asent_en_v1')
 
     # nlp.add_pipe('spacytextblob')
-    # app.run() for localhost
-    app.run(host='0.0.0.0', port=8080)
+    app.run() # for localhost
+    #app.run(host='0.0.0.0', port=8080)
